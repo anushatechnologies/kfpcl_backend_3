828d820dc71ccec331a102c6753cae317df7c05e
powerboysai <velupurisai@gmail.com>
2026-09-12 11:00:34 +0530

    fix(timezone): set JVM and MySQL JDBC default timezone to Asia/Kolkata

--- src/main/java/com/project/kfpcl_exports/KfpclExportsApplication.java
+++ src/main/java/com/project/kfpcl_exports/KfpclExportsApplication.java
@@ -1,12 +1,21 @@
 package com.project.kfpcl_exports;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class KfpclExportsApplication {
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
+
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(KfpclExportsApplication.class, args);
     }
 }
